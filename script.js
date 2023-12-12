12131fe03dbaba29d3ad9425f505ea33d2375018
Saverboss <34922295+Saverboss@users.noreply.github.com>
2023-12-12 11:22:03 +0300

    Update script.js for Interesting-facts GitHubPages

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+    
     // Выбор случайного факта
     const randomFact = facts[Math.floor(Math.random() * facts.length)];
     document.getElementById("factDisplay").innerText = randomFact;
